8d8d2db77488546f4e90728c221e35a6ffd0294e
Eposs <lunalaluna592@gmail.com>
2024-11-29 13:08:06 +0100

    Log real ip in backend

--- backend/src/middleware/logging.ts
+++ backend/src/middleware/logging.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import dayjs from "dayjs";
 
 const logger = (req: Request, res: Response, next: NextFunction) => {
-    console.log(`[${dayjs().format("YYYY-MM-DD:HH:mm:ss")}] Received a ${req.method} request on ${req.path} from ${req.ip}`);
+    console.log(`[${dayjs().format("YYYY-MM-DD:HH:mm:ss")}] Received a ${req.method} request on ${req.path} from ${req.header("X-Real-IP")}`);
     next();
 };
 
