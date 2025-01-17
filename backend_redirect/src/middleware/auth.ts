45366041233626c9a1435d6c2ad407a4ca64bf62
Eposs <lunalaluna592@gmail.com>
2025-01-17 11:42:17 +0100

    Add auth middleware

---
+++ backend_redirect/src/middleware/auth.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from "express";
+
+const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const authHeader = req.headers.authorization;
+    const token = authHeader && authHeader.split(' ')[1];  // Bearer <token>
+
+    if (!token) {
+        res.status(401).send("Token missing");
+        return;
+    }
+
+    const validToken = process.env.TOKEN;
+    if (!validToken) {
+        res.status(500).send("Env missing");
+        return;
+    }
+
+    if (validToken !== token) {
+        res.status(401).send("Invalid Token");
+        return;
+    }
+
+    next();
+}
+
+export default authMiddleware;
